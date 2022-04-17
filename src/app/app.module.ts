577640aa4dbbcdb1ad7e954a7cdc2d9b626b201b
emad <emad.faghihi78@gmail.com>
2022-04-17 17:24:17 +0430

    end-config-ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserTransferStateModule } from '@angular/platform-browser';
+ 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserTransferStateModule
+  ],
+  
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+
+import { IonicServerModule } from '@ionic/angular-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    IonicServerModule,
+    ServerTransferStateModule
   ],
   bootstrap: [AppComponent],
 })
